738196b2e7518f682f21f42fc9cdb5c403b02448
Permana1468 <permana1468@example.com>
2026-07-13 17:21:30 +0700

    fix(prisma): bust globalThis cache for Prisma Client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,12 +10,12 @@ const prismaClientSingleton = () => {
 };
 
 declare global {
-  var prisma_v4: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prisma_v5: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma_v4 ?? prismaClientSingleton();
+const prisma = globalThis.prisma_v5 ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma_v4 = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma_v5 = prisma;
 
